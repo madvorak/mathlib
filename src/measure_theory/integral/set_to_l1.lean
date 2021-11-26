04d82913ff38b6bdbacf806722e40e5d385132ee
RemyDegenne <Remydegenne@gmail.com>
2021-11-26 19:12:35 +0100

    remove duplicates

--- src/measure_theory/integral/set_to_l1.lean
+++ src/measure_theory/integral/set_to_l1.lean
@@ -863,18 +863,6 @@ variables {G'' G' : Type*} [normed_linear_ordered_group G''] [measurable_space G
   [borel_space G''] [second_countable_topology G'']
   [normed_linear_ordered_group G'] [normed_space ℝ G']
 
-lemma Lp.coe_fn_le (f g : Lp G'' p μ) : f ≤ᵐ[μ] g ↔ f ≤ g :=
-by rw [← subtype.coe_le_coe, ← ae_eq_fun.coe_fn_le, ← coe_fn_coe_base, ← coe_fn_coe_base]
-
-lemma Lp.coe_fn_nonneg (f : Lp G'' p μ) : 0 ≤ᵐ[μ] f ↔ 0 ≤ f :=
-begin
-  rw ← Lp.coe_fn_le,
-  have h0 := Lp.coe_fn_zero G'' p μ,
-  split; intro h; filter_upwards [h, h0]; intros a h1 h2,
-  { rwa h2, },
-  { rwa ← h2, },
-end
-
 lemma Lp.simple_func.coe_fn_le (f g : α →₁ₛ[μ] G'') :
   f ≤ᵐ[μ] g ↔ f ≤ g :=
 by rw [← subtype.coe_le_coe, ← Lp.coe_fn_le, coe_fn_coe_base', coe_fn_coe_base' g]
@@ -884,18 +872,6 @@ instance :
 begin
   refine ⟨λ f g₁ g₂ hg₁₂, _⟩,
   rw ← Lp.simple_func.coe_fn_le at hg₁₂ ⊢,
-  have h_add_1 : ⇑(f + g₁) =ᵐ[μ] f + g₁, from Lp.coe_fn_add _ _,
-  have h_add_2 : ⇑(f + g₂) =ᵐ[μ] f + g₂, from Lp.coe_fn_add _ _,
-  filter_upwards [h_add_1, h_add_2, hg₁₂],
-  intros a h1 h2 h3,
-  rw [h1, h2, pi.add_apply, pi.add_apply],
-  exact add_le_add le_rfl h3,
-end
-
-instance TODONAME : covariant_class (Lp G'' 1 μ) (Lp G'' 1 μ) has_add.add has_le.le :=
-begin
-  refine ⟨λ f g₁ g₂ hg₁₂, _⟩,
-  rw ← Lp.coe_fn_le at hg₁₂ ⊢,
   have h_add_1 : ⇑(f + g₁) =ᵐ[μ] f + g₁, from Lp.coe_fn_add _ _,
   have h_add_2 : ⇑(f + g₂) =ᵐ[μ] f + g₂, from Lp.coe_fn_add _ _,
   filter_upwards [h_add_1, h_add_2, hg₁₂],
